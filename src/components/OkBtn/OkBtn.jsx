5ac9b9a7f13b629007daae574d247513b06c9f61
Gustavo Sevilla <gsevillagarces@gmail.com>
2023-03-29 00:44:32 +0200

    Primeros ajustes en FORM de LOGIN

---
+++ src/components/OkBtn/OkBtn.jsx
@@ -0,0 +1,13 @@
+import './OkBtn.css'
+
+export const OkBtn = () => {
+    return(
+        <div className='OkBtn'>
+            <button className='OkBtn-button' type="Button">Click Me!
+                <span className='Button-span'>
+                    <img className='Button-span-img' src="/assets/icons/arrow-right.svg" alt="" />
+                </span>
+            </button>
+        </div>
+    )
+}
--- src/components/login/LoginForm/LoginForm.jsx
+++ src/components/login/LoginForm/LoginForm.jsx
@@ -1,12 +1,17 @@
 import './LoginForm.css'
 import { PasswordField } from '../../PasswordField/PasswordField'
 import { TextField } from '../../TextField/TextField'
+import { OkBtn } from '../../OkBtn/OkBtn'
 
 export const LoginForm = () => {
     return(
         <div className='LoginForm'>
-            < TextField />
-            < PasswordField />
+            <h2 className='LoginForm-h2'>Ingresa tu usuario para iniciar…</h2>
+            <div className='LoginForm-form'>
+                < TextField />
+                < PasswordField />
+            </div>
+                < OkBtn />
         </div>
     )
 }
