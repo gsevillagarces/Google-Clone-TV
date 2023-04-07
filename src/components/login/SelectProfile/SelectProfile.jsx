d7d12ef26b4d2dfd9751d89d5519ddb53f62f951
Gustavo Sevilla <gsevillagarces@gmail.com>
2023-04-07 22:12:12 +0200

    Estilos en Add Profile

--- src/components/login/SelectProfile/SelectProfile.jsx
+++ src/components/login/SelectProfile/SelectProfile.jsx
@@ -4,10 +4,18 @@ import './SelectProfile.css'
 export const SelectProfile = () => {
     return(
         <div className='SelectProfile'>
-            <div className='SelectProfile-profiles'>
-                <div className='SelectProfile-profile' src="/assets/providers/primevideo.svg" alt="Google Clone Profile"></div>
-                <div className='SelectProfile-profile' src="/assets/providers/primevideo.svg" alt="Google Clone Profile"></div>
-                <div className='SelectProfile-profile' src="/assets/providers/primevideo.svg" alt="Google Clone Profile"></div>
+            <div className='SelectProfile-container'>
+                <div className='SelectProfile-profiles'>
+                    <img className='SelectProfile-profile' src="/assets/imgs/user.jpg" alt="Google Clone Profile" />
+                    <img className='SelectProfile-profile' src="/assets/imgs/user.jpg" alt="Google Clone Profile" />
+                </div>
+                <div className='SelectProfile-addProfiles'>
+                    <div className='SelectProfile-profile SelectProfile-addProfile' src="/assets/providers/primevideo.svg" alt="Google Clone Profile">
+                    <span>
+                        <img className='SelectProfile-addProfile-img' src="/assets/icons/add.svg" alt="Add User" />
+                    </span>
+                    </div>
+                </div>
             </div>
 
             <div className='SelectProfile-footer'>
