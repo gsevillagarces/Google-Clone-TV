7a535facff765eefed7bb201504ee548a2c46c5c
Gustavo Sevilla <gsevillagarces@gmail.com>
2023-04-23 16:36:55 +0200

    Cambios en CAROUSELS de Apps

--- src/pages/ForYou/ForYou.jsx
+++ src/pages/ForYou/ForYou.jsx
@@ -3,15 +3,42 @@ import './ForYou.css'
 import { Slider } from '../../components/home/Slider/Slider'
 import { CarouselMovies } from '../../components/CarouselMovies/CarouselMovies'
 import { CarouselApps } from '../../components/CarouselApps/CarouselApps'
+import { useEffect, useState } from 'react'
 
-export const ForYou = () => {
+export const ForYou = ( filter ) => {
+
+  const [ apps, setApps ] = useState([])
+
+  useEffect ( () => {
+
+      let controller = new AbortController()
+      console.log( controller )
+
+      let options = {
+          method: "get",
+          signal : controller.signal,
+          headers : {
+              "Content-type" : "application/json"
+          } 
+      }
+
+      fetch( 'http://localhost:4002/apps', options )
+      .then( res => res.json() )
+      .then( data => setApps(data) )
+      .catch( err => setApps(err) )
+      .finally( () => controller.abort() )
+
+      console.log(apps)
+      
+  }, [])
+
     return(
         <div className='ForYou'>
           < Slider />
           <h2 className='CloneTV-h2'>Top picks</h2>
             < CarouselMovies />
           <h2 className='CloneTV-h2'>Your apps</h2>
-            < CarouselApps />
+            < CarouselApps apps = { apps } filter = "y" />
           <h2 className='CloneTV-h2'>Keep watching</h2>
             < CarouselMovies />
           <h2 className='CloneTV-h2'>On your watchlist</h2>
